7a6796fec0f2e22b2e87e0275159c98a3d1c1cc4
cuyssi <anabcastrolopez@gmail.com>
2025-07-01 22:56:56 +0200

    add /ping route for wake-up

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import translation, dictionary, suggestion, admin
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+@app.get("/ping")
+def ping():
+    return JSONResponse(content={"message": "pong"})
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,7 +19,5 @@ app.add_middleware(
 
 app.include_router(translation.router)
 app.include_router(dictionary.router)
-app.include_router(
-    suggestion.router, prefix="/sugerencias"
-)  # ✅ ya tiene el prefix correcto
+app.include_router(suggestion.router, prefix="/sugerencias")
 app.include_router(admin.router, prefix="/admin")
